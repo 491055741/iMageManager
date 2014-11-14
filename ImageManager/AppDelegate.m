cf8a1efe6973cf039c94fcdfcf06c2ff09226cb2
491055741 <491055741@qq.com>
2014-11-15 00:34:35 +0800

    disable auto snapshot

--- ImageManager/AppDelegate.m
+++ ImageManager/AppDelegate.m
@@ -52,13 +52,15 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    self.window.hidden = YES; // forbidden auto snapshot
     [VideoThumbImageView flush];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-
+    self.window.hidden = NO;
     [self showLoginView];
 //    _navController.topViewController.navigationController.navigationBar.hidden = NO;
     _navController.topViewController.view.hidden = NO;
